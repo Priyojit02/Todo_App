b9c0e2d55b5ddc64d11217513c2d05dd305f75f5
Priyojit02 <138701415+Priyojit02@users.noreply.github.com>
2024-06-05 13:24:06 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import AppName from "./Components/AppName";
+import AddTodo from "./Components/AddTodo";
+import TodoItem1 from "./Components/TodoItem1";
+import TodoItem2 from "./Components/TodoItem2"
+import "./App.css";
+
+function App() {
+  return (
+    <center className="todo-container">
+      <AppName></AppName>
+      
+      <div className="items-container">
+        <AddTodo></AddTodo> 
+        <TodoItem1></TodoItem1>
+        <TodoItem2></TodoItem2>
+      </div>
+    </center>
+  );
+}
+
+
+
+export default App
